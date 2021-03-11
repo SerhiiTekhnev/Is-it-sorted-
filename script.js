4f6ef6ce816f08ad1eae30dacc597079ab227f9c
SerhiiTekhnev <seolteh@gmail.com>
2021-03-11 13:37:47 +0200

    var.1

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let button = document.querySelector('button[type=submit]')
 button.addEventListener('click', function() {
     let str = document.querySelector('form input[name=word]').value;
     let arr = str.split(',').map(Number);
-    // let arrSorted = str.split(',').map(Number).sort();
     let result;
 
     if (arr.length === 1) {
@@ -20,33 +19,4 @@ button.addEventListener('click', function() {
     }}
     console.log(arr);
     alert(result);
-    
-   
 })
-
-
-
-
-// let str = document.querySelector('form input[name=word]').value;
-//     let arr = str.split(',').map(Number);
-//     // let arrSorted = str.split(',').sort();
-//     let result;
-
-//     // if (arr = [0]) {
-//     //     result = 'TRUE';
-//     // }
-
-//     for (let i = 1; i <= arr.length - 1; i++) {
-
-//         if (arr[i - 1] < arr[i]) {
-//             result = 'TRUE'; 
-//         } else {
-//             result = 'FALSE';
-//             break;
-//         }
-//     }
-//     console.log(arr);
-//     alert(result);
-    
-   
-// })
